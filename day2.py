09b4aa8508d2765a9a70f5031e60802789372e5f
Bret Gourdie <bretgourdie@gmail.com>
2018-01-22 10:34:56 -0500

    Solved second part of day2

--- day2.py
+++ day2.py
@@ -1,15 +1,36 @@
 import csv
+from enum import Enum
 
-def getChecksum():
+class ChecksumStrategy(Enum):
+    MinMaxDiff = 1,
+    EvenDivide = 2
+
+def getEvenDivide(row):
+    for num1 in row:
+        for num2 in row:
+            if num1 != num2:
+                iMax = max(num1, num2)
+                iMin = min(num1, num2)
+                if iMax // iMin == iMax / iMin:
+                    return iMax // iMin
+
+def getChecksumInput(row, strategy):
+    if strategy == ChecksumStrategy.MinMaxDiff:
+        return max(row) - min(row)
+    elif strategy == ChecksumStrategy.EvenDivide:
+        return getEvenDivide(row)
+
+def getChecksum(strategy):
     with open("input.txt", "r") as csvfile:
         ss = csv.reader(csvfile, delimiter="\t")
-        diffs = []
+        checksumInputs = []
 
         for strRow in ss:
             row = list(map(int, strRow))
-            diff = max(row) - min(row)
-            diffs.append(diff)
+            checksumInput = getChecksumInput(row, strategy)
+            checksumInputs.append(checksumInput)
 
-        return sum(diffs)
+        return sum(checksumInputs)
 
-print(getChecksum())
+print(getChecksum(ChecksumStrategy.MinMaxDiff))
+print(getChecksum(ChecksumStrategy.EvenDivide))
