3a20b80b215051640bece523fe15675cae4c2f14
Bret Gourdie <bretgourdie@gmail.com>
2018-04-12 13:23:06 -0400

    Completed day4 part 2

--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+def isAnagram(str1, str2):
+    return sorted(str1) == sorted(str2)
+
 passphrases = []
 
 with open("day4-input.txt", "r") as passphraseFile:
@@ -12,4 +15,22 @@ for passphrase in passphrases:
     if len(passwords) == len(passwordsSet):
         correctPassphrases += 1
 
-print("Number of correct passphrases: {}".format(correctPassphrases))
+print("Number of correct passphrases (part 1): {}".format(correctPassphrases))
+
+correctPassphrases = 0
+for passphrase in passphrases:
+    hasAnagram = False
+    passwords = passphrase.split()
+
+    for mainPasswordIndex in range(0, len(passwords)):
+        mainPassword = passwords[mainPasswordIndex]
+
+        for comparingPasswordIndex in range(0, len(passwords)):
+            if mainPasswordIndex != comparingPasswordIndex:
+                comparingPassword = passwords[comparingPasswordIndex]
+                hasAnagram = hasAnagram or isAnagram(mainPassword, comparingPassword)
+
+    if not hasAnagram:
+        correctPassphrases += 1
+
+print("Number of correct passphrases (part 2): {}".format(correctPassphrases))
