4584367927c5926d91836b96ab90eb39be140897
Bret Gourdie <bretgourdie@gmail.com>
2018-04-16 13:15:36 -0400

    Solved day6 part 1

---
+++ day6.py
@@ -0,0 +1,41 @@
+def getMemoryBankWithMostBlocks(memoryBank):
+    maxBlocks = -1
+    maxBlocksIndex = 0
+    for index in range(0, len(memoryBank)):
+        currentBlocks = memoryBank[index]
+
+        if currentBlocks > maxBlocks:
+            maxBlocksIndex = index
+            maxBlocks = currentBlocks
+
+    return maxBlocksIndex
+
+def getNextMemoryBankIndex(memoryBank, index):
+    return (index + 1) % len(memoryBank)
+
+def redistributeBlocks(memoryBank):
+    indexToRedistribute = getMemoryBankWithMostBlocks(memoryBank)
+    blocksToRedistribute = memoryBank[indexToRedistribute]
+    memoryBank[indexToRedistribute] = 0
+
+    index = getNextMemoryBankIndex(memoryBank, indexToRedistribute)
+    while blocksToRedistribute > 0:
+        memoryBank[index] += 1
+        blocksToRedistribute -= 1
+        index = getNextMemoryBankIndex(memoryBank, index)
+
+memoryBank = []
+with open("input.txt", "r") as memoryBankFile:
+    rawLine = memoryBankFile.readline()
+    memoryBank = [int(i) for i in rawLine.split()]
+
+numberOfRedistributions = 0
+memoryBankConfigurations = []
+
+while memoryBank not in memoryBankConfigurations:
+    memoryBankConfigurations.append(memoryBank[:])
+    
+    redistributeBlocks(memoryBank)
+    numberOfRedistributions += 1
+
+print("Took {} redistributions to find a loop".format(numberOfRedistributions))
