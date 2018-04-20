a9afe2b465e8260eca74f13b4280f7c8ebab032a
Bret Gourdie <bretgourdie@gmail.com>
2018-04-19 22:45:45 -0400

    Solved day 8 part 1

---
+++ day8.py
@@ -0,0 +1,60 @@
+def getLargestRegisterValue(valueByRegister):
+    return max(v for k, v in valueByRegister.items())
+
+def getMultiplicand(action):
+    if action == "inc":
+        return 1
+    elif action == "dec":
+        return -1
+    return 0
+
+def conditionSuccessful(registerValue, conditionSign, immediate):
+    if conditionSign == ">":
+        return registerValue > immediate
+    elif conditionSign == "<":
+        return registerValue < immediate
+    elif conditionSign == ">=":
+        return registerValue >= immediate
+    elif conditionSign == "<=":
+        return registerValue <= immediate
+    elif conditionSign == "==":
+        return registerValue == immediate
+    elif conditionSign == "!=":
+        return registerValue != immediate
+
+    print("Weird condition \"{}\"".format(conditionSign))
+    return None
+
+instructions = []
+with open("input.txt", "r") as instructionFile:
+    instructions = instructionFile.readlines()
+
+valueByRegister = {}
+
+for instruction in instructions:
+    splitIn = instruction.split()
+    
+    targetRegister = splitIn[0]
+    action = splitIn[1]
+    targetImmediate = int(splitIn[2])
+    ifWord = splitIn[3]
+    sourceRegister = splitIn[4]
+    conditionSign = splitIn[5]
+    sourceImmediate = int(splitIn[6])
+
+    multiplicand = getMultiplicand(action)
+
+    if sourceRegister not in valueByRegister:
+        valueByRegister[sourceRegister] = 0
+
+    sourceRegisterValue = valueByRegister[sourceRegister]
+
+    if conditionSuccessful(sourceRegisterValue, conditionSign, sourceImmediate):
+        targetAddor = targetImmediate * getMultiplicand(action)
+
+        if targetRegister not in valueByRegister:
+            valueByRegister[targetRegister] = 0
+
+        valueByRegister[targetRegister] += targetAddor
+
+print("Largest value in any register is {}".format(getLargestRegisterValue(valueByRegister)))
