b2666ce2fb5e7ff76eb60a856b99a079fa022fea
Bret Gourdie <bretgourdie@gmail.com>
2018-04-22 19:15:23 -0400

    Solved day 11 (part 1 and 2)

--- day11.py
+++ day11.py
@@ -1,29 +1,40 @@
 def getMovement(notation):
     if notation == "n":
-        return (0, -1)
+        return (0, 1, -1)
     elif notation == "nw":
-        return (-1, 0)
+        return (-1, 1, 0)
     elif notation == "ne":
-        return (1, -1)
+        return (1, 0, -1)
     elif notation == "s":
-        return (0, 1)
+        return (0, -1, 1)
     elif notation == "sw":
-        return (-1, 1)
+        return (-1, 0, 1)
     elif notation == "se":
-        return (1, 0)
+        return (1, -1, 0)
     else:
-        return (None, None)
+        return (None, None, None)
 
 steps = []
 with open("input.txt", "r") as stepsFile:
     steps = [step.strip() for step in stepsFile.readline().split(",")]
 
-q, r = (0, 0)
+x, y, z = (0, 0, 0)
+
+furthestEverGot = 0
 
 for step in steps:
-    newQ, newR = getMovement(step)
-    q += newQ
-    r += newR
+    moveX, moveY, moveZ = getMovement(step)
+    x += moveX
+    y += moveY
+    z += moveZ
+    
+    currentCoords = [abs(x), abs(y), abs(z)]
+    
+    if max(currentCoords) > furthestEverGot:
+        furthestEverGot = max(currentCoords)
 
-stepsAway = None
+coords = [abs(x), abs(y), abs(z)]
+
+stepsAway = max(coords)
 print("Child is {} steps away".format(stepsAway))
+print("Furthest child ever got was {} steps away".format(furthestEverGot))
