bf16b53eb5ffc39b6652d3e7c5e821041e92262f
Bret Gourdie <bretgourdie@gmail.com>
2018-04-21 20:37:34 -0400

    Solved day10 part 1

---
+++ day10.py
@@ -0,0 +1,26 @@
+
+lengths = []
+with open("input.txt", "r") as lengthsFile:
+    lengths = [int(x) for x in lengthsFile.readline().split(",")]
+
+numElements =256 
+string = [x for x in range(numElements)]
+
+currentPosition = 0
+skipSize = 0
+for length in lengths:
+
+    slice = []
+    for indexOffset in range(length):
+        moddedIndexOffset = (currentPosition + indexOffset) % len(string)
+        slice.append(string[moddedIndexOffset])
+
+    for indexOffset in range(length):
+        moddedIndexOffset = (currentPosition + indexOffset) % len(string)
+        string[moddedIndexOffset] = slice[::-1][indexOffset]
+
+    currentPosition += length + skipSize
+
+    skipSize += 1
+
+print("First two numbers multiplied are {}".format(string[0] * string[1]))
