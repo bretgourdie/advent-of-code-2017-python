0c95069db6c951b5e080c03d047678d775c48722
Bret Gourdie <bretgourdie@gmail.com>
2018-04-27 15:24:03 -0400

    Implemented part 2; need to fix knothash

--- day14.py
+++ day14.py
@@ -1,5 +1,30 @@
 from knothash import KnotHash
 
+def floodFill(coordinate, groupState, groupNumber):
+    global coordsToGroup
+    global grid
+
+    if coordinate not in coordsToGroup:
+        x, y = coordinate
+        if x >= 0 and x < len(grid) and y >= 0 and y < len(grid):
+            cellState = grid[x][y]
+
+            if cellState == groupState:
+                coordsToGroup[coordinate] = groupNumber
+
+                floodFill((x-1, y), groupState, groupNumber) # north
+                floodFill((x+1, y), groupState, groupNumber) # south
+                floodFill((x, y+1), groupState, groupNumber) # east
+                floodFill((x, y-1), groupState, groupNumber) # west
+
+def printGrid(grid):
+    with open("gridFile.txt", "w") as gridFile:
+        for rowIndex, row in enumerate(grid):
+            for colIndex, col in enumerate(row):
+                toPrint = "#" if col else "."
+                gridFile.write(toPrint)
+            gridFile.write("\n")
+
 inputStr = ""
 with open("input.txt", "r") as inputFile:
     inputStr = inputFile.readline().strip()
@@ -13,13 +38,29 @@ for row in range(dimension):
     
     currentKnotHash = knothash.generateHash(currentKnotHashInput)
     decimalKnotHash = int(currentKnotHash, 16)
-    binaryKnotHash = bin(decimalKnotHash)
+    rawBinaryKnotHash = bin(decimalKnotHash)
+    no0xBinaryKnotHash = rawBinaryKnotHash[2:]
+    binaryKnotHash = no0xBinaryKnotHash + "0" * (dimension - len(no0xBinaryKnotHash))
 
     gridRow = []
-    for digit in str(binaryKnotHash)[2:]:
+    for digit in str(binaryKnotHash):
         gridRow.append(digit == "1")
         usedSquares += digit == "1" if 1 else 0
 
     grid.append(gridRow)
 
 print("Number of used squares: {}".format(usedSquares))
+
+printGrid(grid)
+
+coordsToGroup = {}
+groupNumber = 0
+for rowIndex, row in enumerate(grid):
+    for colIndex, col in enumerate(row):
+         coordinate = (rowIndex, colIndex)
+
+         if coordinate not in coordsToGroup:
+             floodFill(coordinate, col, groupNumber)
+             groupNumber += 1
+
+print("Number of groups: {}".format(groupNumber))
