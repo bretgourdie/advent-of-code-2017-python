4d540cf5c7191bd30ba800e23e938dd7d9d4eef0
Bret Gourdie <bretgourdie@gmail.com>
2018-05-03 13:19:14 -0400

    Finished day17 for now (unsolved)

--- day17.py
+++ day17.py
@@ -4,6 +4,7 @@ with open("input.txt", "r") as stepsFile:
 numberOfRuns = 2017
 buffer = [0]
 currentPosition = 0
+run = 1
 for run in range(1, numberOfRuns + 1):
     currentPosition = (currentPosition + steps) % len(buffer) + 1
     buffer.insert(currentPosition, run)
@@ -12,3 +13,18 @@ nextPosition = (currentPosition + 1) % len(buffer)
 valueAfterNumberOfRuns = buffer[nextPosition]
 
 print("Value after {} is {}".format(numberOfRuns, valueAfterNumberOfRuns))
+
+numberOfRuns = 50000000
+while run <= steps:
+    currentPosition = (currentPosition + steps) % len(buffer) + 1
+    buffer.insert(currentPosition, run)
+    run += 1
+
+
+
+targetNumber = 0
+indexOfTargetNumber = buffer.index(targetNumber)
+nextPosition = (indexOfTargetNumber + 1) % len(buffer)
+valueAfterTargetNumber = buffer[nextPosition]
+
+print("Value after {} is {}".format(targetNumber, valueAfterTargetNumber))
