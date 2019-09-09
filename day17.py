7af4cacd1eb7e4417cc59fd46435ed4c682c19be
Bret Gourdie <BretGourdie@gmail.com>
2019-09-09 15:19:34 -0400

    Refactored for clarity

--- day17.py
+++ day17.py
@@ -1,42 +1,42 @@
 class Part:
-    def __init__(self, runs, targetIndex, strategy):
+    def __init__(self, runs, target, strategy):
         self.runs = runs
-        self.targetIndex = targetIndex
+        self.target = target
         self.strategy = strategy
 
 with open("input.txt", "r") as stepsFile:
     stepsAfterRun = int(stepsFile.readline())
 
-def firstQuestionAnswerStrategy(buffer, targetNumber):
+def firstPartStrategy(buffer, targetNumber):
     targetNumberIndex = buffer.index(targetNumber)
 
     return buffer[targetNumberIndex + 1]
 
-def secondQuestionAnswerStrategy(buffer, targetIndex):
+def secondPartStrategy(buffer, targetIndex):
     return buffer[targetIndex]
 
 def printAnswer(numberOfRuns, targetIndex, answer):
     print("The number at {} after {} runs is {}".format(targetIndex, numberOfRuns, answer))
 
-def spin(numberOfRuns, targetIndex, stepsAfterRun, answerStrategy):
+def spin(numberOfRuns, maxBufferLength, stepsAfterRun, answerStrategy):
     buffer = []
     currentIndex = 0
     
     for run in range(0, numberOfRuns + 1):
         buffer.insert(currentIndex, run)
 
-        if len(buffer) > targetIndex + 1:
+        if len(buffer) > maxBufferLength + 1:
             buffer.pop()
 
         currentIndex = ((currentIndex + stepsAfterRun) % (run + 1)) + 1
 
-    return answerStrategy(buffer, targetIndex)
+    return answerStrategy(buffer, maxBufferLength)
 
 parts = [
-    Part(2017, 2017, firstQuestionAnswerStrategy),
-    Part(50000000, 1, secondQuestionAnswerStrategy)
+    Part(2017, 2017, firstPartStrategy),
+    Part(50000000, 1, secondPartStrategy)
 ]
 
 for part in parts:
-    result = spin(part.runs, part.targetIndex, stepsAfterRun, part.strategy)
-    printAnswer(part.runs, part.targetIndex, result)
+    result = spin(part.runs, part.target, stepsAfterRun, part.strategy)
+    printAnswer(part.runs, part.target, result)
