6b36390350b39c8c8d12811de6a90d253dce5f84
Bret Gourdie <BretGourdie@gmail.com>
2019-09-11 00:33:44 -0400

    Runs through, does not rcv ever

--- day18.py
+++ day18.py
@@ -33,46 +33,76 @@ class Duet:
         self.shouldIncrement = not cameFromJgz
 
     def __initRegister(self, register):
+        if not register.isalpha():
+            return
+        
         if register not in self.registers:
             self.registers[register] = 0
 
     def __getValueFromRegisterOrImmediate(self, value):
         if value in self.registers:
-            return registers[value]
+            return self.registers[value]
         else:
-            return value
+            return int(value)
 
-    def snd(self, value):
+    def __getValue(self, valueOrRegister):
+        self.__initRegister(valueOrRegister)
+        return self.__getValueFromRegisterOrImmediate(valueOrRegister)
+
+    def snd(self, valueOrRegister):
+        value = self.__getValue(valueOrRegister)
         self.sndAndRcvStrategy.snd(value)
 
-    def set(self, register, value):
+    def set(self, register, valueOrRegister):
+        self.__initRegister(register)
+        value = self.__getValue(valueOrRegister)
         self.registers[register] = value
 
-    def add(self, register, value):
-        __initRegister(register)
+    def add(self, register, valueOrRegister):
+        self.__initRegister(register)
+        value = self.__getValue(valueOrRegister)
         self.registers[register] += value
 
-    def mul(self, register, value):
-        ___initRegister(register)
+    def mul(self, register, valueOrRegister):
+        self.__initRegister(register)
+        value = self.__getValue(valueOrRegister)
         self.registers[register] *= value
 
-    def mod(self, register, value):
-        __initRegister(register)
+    def mod(self, register, valueOrRegister):
+        self.__initRegister(register)
+        value = self.__getValue(valueOrRegister)
         self.registers[register] = registers[register] % value
 
-    def rcv(self, value):
+    def rcv(self, valueOrRegister):
+        value = self.__getValue(valueOrRegister)
         self.sndAndRcvStrategy.rcv(value)
 
-    def jgz(self, greaterThanZeroCheck, offset):
-        if greaterThanZeroCheck > 0:
+    def jgz(self, valueOrRegister, offsetValueOrRegister):
+        value = self.__getValue(valueOrRegister)
+        offset = self.__getValue(valueOrRegister)
+        if value > 0:
             self.index += offset
 
     def process(self):
         while self.index >= 0 and self.index < len(self.instructions):
-            instructionLine = self.instructions[index]
+            instructionLine = self.instructions[self.index]
             splitInstruction = instructionLine.split()
             instruction = splitInstruction[0]
 
             function = self.instructionToFunction[instruction]
-            function(splitInstruction[1:])
+            function(*splitInstruction[1:])
 
+            if self.__handleShouldIncrement:
+                self.index += 1
+
+    def getRecoveredFrequency(self):
+        return self.sndAndRcvStrategy.recoveredFrequency
+
+with open("input.txt", "r") as instructionsFile:
+    instructions = instructionsFile.readlines()
+
+duet = Duet(instructions, SoundAndRecover())
+
+duet.process()
+
+print("The last recovered frequency is {}".format(duet.getRecoveredFrequency()))
