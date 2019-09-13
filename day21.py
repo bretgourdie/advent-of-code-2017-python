c728832d3e9a5d1854fc4678599768f602a2e031
Bret Gourdie <BretGourdie@gmail.com>
2019-09-13 13:56:09 -0400

    Start work on day 21 part 1

--- day21.py
+++ day21.py
@@ -1,1 +1,22 @@
+class Shape():
+    def getTransforms(self, form):
+        transforms = [form]
 
+        for i in range(3):
+            # fix rotations
+            pass
+
+        flippedList = [l[::-1] for l in form]
+
+        transforms.append(flippedList)
+
+        return transforms
+
+    def __init__(self, line):
+        self.form = line.split("/")
+        self.size = len(self.form)
+        self.transforms = self.getTransforms(self.form)
+
+startingShape = Shape(".#./..#/###")
+
+print(startingShape.form)
