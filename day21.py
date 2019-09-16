a9185664b0d41078badca9203a622e54ac5483bf
Bret Gourdie <BretGourdie@gmail.com>
2019-09-16 14:41:19 -0400

    Transform to lines

--- day21.py
+++ day21.py
@@ -17,10 +17,16 @@ class Shape():
 
         return transforms
 
+    def getLineTransforms(self, transforms):
+        return ["/".join(x) for x in transforms]
+
     def __init__(self, line):
+        self.line = line
         self.form = line.split("/")
         self.size = len(self.form)
         self.transforms = self.getTransforms(self.form)
+        self.allLines = self.getLineTransforms(self.transforms)
 
 startingShape = Shape(".#./..#/###")
 
+pass
